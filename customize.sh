06420c25a49b368e8d766eec8fa2add4127de6a9
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2019-12-17 12:06:03 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,4 +6,4 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/12.0.0.20/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/12.0.0.20/g' openwrt/package/base-files/files/bin/config_generate
