f27b298d4b3c6577b1a8ee462751a4b320f8b961
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2019-12-26 17:27:21 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,11 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# 定制默认 IP
-sed -i 's/192.168.1.1/12.0.0.20/g' openwrt/package/base-files/files/bin/config_generate
+
+# 默认IP
+sed -i 's/192.168.1.1/12.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
+
 # 添加第三方插件
 git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
