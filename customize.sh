28997954ed27bda4e07b9efd697e8da6f25ca2c0
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2019-12-26 15:38:46 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,3 +8,5 @@
 #=================================================
 # 定制默认 IP
 sed -i 's/192.168.1.1/12.0.0.20/g' openwrt/package/base-files/files/bin/config_generate
+# 添加第三方插件
+git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
