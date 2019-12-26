66c30b76f1fe90a9b44567e3b695ad916fea9830
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2019-12-26 20:11:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,6 +11,6 @@
 sed -i 's/192.168.1.1/12.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 添加第三方插件
-git clone https://github.com/KFERMercer/luci-app-serverchan package/lean/luci-app-serverchan
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
-# git clone https://github.com/kang-mk/luci-app-smartinfo package/lean/luci-app-smartinfo
+git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
