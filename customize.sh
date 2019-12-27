c451381975e16fc8130e0714439fe552afb91198
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2019-12-27 18:14:02 +0800

    替换默认Argon主题

--- customize.sh
+++ customize.sh
@@ -10,6 +10,10 @@
 # 定制默认IP
 sed -i 's/192.168.1.1/12.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 
+# 替换默认Argon主题
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
 # 添加第三方软件包
 git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
