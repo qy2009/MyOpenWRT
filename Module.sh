2e654dc4fcea190a181c3d61c6b5b3dc47352cee
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-01-04 15:27:05 +0800

    Update Module.sh

--- Module.sh
+++ Module.sh
@@ -12,9 +12,9 @@ sed -i 's/192.168.1.1/12.0.0.1/g' openwrt/package/base-files/files/bin/config_ge
 
 # 替换默认Argon主题
 rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon package/openwrt-packages/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # 添加第三方软件包
-git clone https://github.com/KFERMercer/luci-app-serverchan package/openwrt-packages/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/openwrt-packages/luci-app-adguardhome
-git clone https://github.com/kang-mk/luci-app-smartinfo package/openwrt-packages/luci-app-smartinfo
+git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
