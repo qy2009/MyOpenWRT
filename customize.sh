be2bd093db651aa28da410c6508f901088d61c99
qy2009 <51539377+qy2009@users.noreply.github.com>
2020-04-11 16:07:05 -0700

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's#192.168.1.1#10.0.0.1#g' package/base-files/files/bin/config_generate
